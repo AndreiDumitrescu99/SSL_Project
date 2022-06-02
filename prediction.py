7679d4607db88a5fe0794a66b9dc42e17ebc4661
AndreiDumitrescu99 <andreidumitrescu99@gmail.com>
2022-06-02 11:31:54 +0300

    Final Milestone

--- prediction.py
+++ prediction.py
@@ -1,16 +1,25 @@
 from tkinter import BASELINE
+from pytz import VERSION
 import tensorflow as tf
+import torch as th
 import numpy as np
 from preprocess import get_sentence_embeds_for_dataset
-from data_reader import read_dataset_convabuse, read_dataset_tcc, read_dataset_alyt
+from data_reader import read_dataset_convabuse, read_dataset_tcc, read_dataset_alyt, get_dataset_bert
+from second_iteration import Net, Net2
+
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+device = th.device('cuda:0' if th.cuda.is_available() else 'cpu')
 
-DATASET = 'ALYT'
-MODEL_PATH = './models/bilstm_attention_alyt'
+DATASET = 'ConvAbuse'
+MODEL_PATH = './models_torch/convabuse_combined_hatebert2.pt'
 TARGETS = 2
-BASELINE = False
+VERSION = 'second_iteration' # This should be one of the following: 'baseline', 'first_iteration', 'second_iteration'.
+
+"""THIS ARE ONLY FOR PYTORCH EXPERIMENTS"""
+INPUTS = 768
+MODEL_VERSION = 2
 
 if __name__ == "__main__":
 
@@ -27,36 +36,77 @@ if __name__ == "__main__":
     x_test = []
     y_test = []
 
-    if BASELINE == True:
+    if VERSION == 'baseline':
         x_test, y_test = get_sentence_embeds_for_dataset(test_set, nn=True, targets=TARGETS, average=True)
     else:
-        x_test, y_test = get_sentence_embeds_for_dataset(test_set, targets=TARGETS, average=False)
+        if VERSION == 'first_iteration':
+            x_test, y_test = get_sentence_embeds_for_dataset(test_set, targets=TARGETS, average=False)
+        else:
+            x_test, y_test, max_len = get_dataset_bert(test_set, TARGETS)
 
-    loaded_model = tf.keras.models.load_model(MODEL_PATH)
-    predictions = loaded_model.predict(np.array(x_test), verbose=1)
+
+    if VERSION == 'second_iteration':
+        if MODEL_VERSION == 2:
+            net = Net2(INPUTS, TARGETS)
+        else:
+            net = Net(INPUTS, TARGETS)
+
+        net.load_state_dict(th.load(MODEL_PATH))
+        net.to(device)
+    else:
+        loaded_model = tf.keras.models.load_model(MODEL_PATH)
+        predictions = loaded_model.predict(np.array(x_test), verbose=1)
 
     cnt = 0
     sem_wrong = 0
     sem_correct = 0
-    for p in predictions:
-        outcome = np.argmax(p)
-        real_outcome = np.argmax(y_test[cnt])
-        
-        if outcome != real_outcome and sem_wrong == 0:
-            print("~~~~~ Wrong Sample ~~~~~")
-            print("Text:")
-            print(test_set[cnt][0])
-            print("Real Outcome: ", real_outcome, " Our outcome: ", outcome)
-            sem_wrong = 1
 
-        if outcome == real_outcome and sem_correct == 0:
-            print("~~~~~ Correct Sample ~~~~~")
-            print("Text:")
-            print(test_set[cnt][0])
-            print("Real Outcome: ", real_outcome, " Our outcome: ", outcome)
-            sem_correct = 1
-        
-        if sem_correct == 1 and sem_wrong == 1:
-            break
+    if VERSION == 'second_iteration':
+        for i, x in enumerate(x_test):
+            x = th.tensor([x]).to(device)
 
-        cnt = cnt + 1
+            y_out = net.forward(x).cpu().detach().numpy()
+
+            y_out = np.argmax(y_out)
+
+            if np.argmax(y_test[i]) != y_out and sem_wrong == 0:
+                print("~~~~~ Wrong Sample ~~~~~")
+                print("Text:")
+                print(test_set[cnt][0])
+                print("Real Outcome: ", np.argmax(y_test[i]), " Our outcome: ", y_out)
+                sem_wrong = 1
+            
+            if np.argmax(y_test[i]) == y_out and sem_correct == 0:
+                print("~~~~~ Correct Sample ~~~~~")
+                print("Text:")
+                print(test_set[cnt][0])
+                print("Real Outcome: ", np.argmax(y_test[i]), " Our outcome: ", y_out)
+                sem_correct = 1
+            
+            if sem_correct == 1 and sem_wrong == 1:
+                break
+
+            cnt = cnt + 1
+    else: 
+        for p in predictions:
+            outcome = np.argmax(p)
+            real_outcome = np.argmax(y_test[cnt])
+            
+            if outcome != real_outcome and sem_wrong == 0:
+                print("~~~~~ Wrong Sample ~~~~~")
+                print("Text:")
+                print(test_set[cnt][0])
+                print("Real Outcome: ", real_outcome, " Our outcome: ", outcome)
+                sem_wrong = 1
+
+            if outcome == real_outcome and sem_correct == 0:
+                print("~~~~~ Correct Sample ~~~~~")
+                print("Text:")
+                print(test_set[cnt][0])
+                print("Real Outcome: ", real_outcome, " Our outcome: ", outcome)
+                sem_correct = 1
+            
+            if sem_correct == 1 and sem_wrong == 1:
+                break
+
+            cnt = cnt + 1
